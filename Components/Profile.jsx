c4f66ce4b143c96b4027260a38bf9dac9c925992
adamelmohamad <37544116+adamelmohamad@users.noreply.github.com>
2021-11-26 16:38:08 +1100

    .

--- Components/Profile.jsx
+++ Components/Profile.jsx
@@ -182,7 +182,7 @@ export default function Profile ({navigation}){
                                     info['badges'].map((badge, i) => {
                                         return (
                                             <Text style={{textAlign:'center', width:'min-content'}} key={i}>
-                                                <Image source={require(`../icons/badges/${badge}.PNG`)} style={{width:'150px', height:'150px'}}/>
+                                                <Image source={require(`../public/icons/badges/${badge}.PNG`)} style={{width:'150px', height:'150px'}}/>
                                             </Text>
                                         )
                                     })
@@ -192,7 +192,7 @@ export default function Profile ({navigation}){
                                     unearnt_badges.map((badge, i) => {
                                             return (
                                                 <Text style={{textAlign:'center', width:'min-content'}} key={i}>
-                                                    <Image source={require(`../icons/badges/${badge}_unearnt.PNG`)} style={{width:'150px', height:'150px'}}/>
+                                                    <Image source={require(`../public/icons/badges/${badge}_unearnt.PNG`)} style={{width:'150px', height:'150px'}}/>
                                                     {getCaptionforBadge(badge)}
                                                 </Text>
                                             )

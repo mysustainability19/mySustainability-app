bd6074f252f744a1159db0572fe4d592dbbe6608
adamelmohamad <37544116+adamelmohamad@users.noreply.github.com>
2021-12-11 08:44:33 +1100

    .

--- Components/Add.jsx
+++ Components/Add.jsx
@@ -149,23 +149,23 @@ export default function Add ({route, navigation}){
                                         onValueChange={(value) =>
                                             setSelected1stSDG(value)
                                         }>
-                                        <Picker.Item label="1" value="1" />
-                                        <Picker.Item label="2" value="2" />
-                                        <Picker.Item label="3" value="3" />
-                                        <Picker.Item label="4" value="4" />
-                                        <Picker.Item label="5" value="5" />
-                                        <Picker.Item label="6" value="6" />
-                                        <Picker.Item label="7" value="7" />
-                                        <Picker.Item label="8" value="8" />
-                                        <Picker.Item label="9" value="9" />
-                                        <Picker.Item label="10" value="10" />
-                                        <Picker.Item label="11" value="11" />
-                                        <Picker.Item label="12" value="12" />
-                                        <Picker.Item label="13" value="13" />
-                                        <Picker.Item label="14" value="14" />
-                                        <Picker.Item label="15" value="15" />
-                                        <Picker.Item label="16" value="16" />
-                                        <Picker.Item label="17" value="17" />
+                                        <Picker.Item label="SDG 1 - No Poverty" value="1" />
+                                        <Picker.Item label="SDG 2 - Zero Hunger" value="2" />
+                                        <Picker.Item label="SDG 3 - Good Health and Well-Being" value="3" />
+                                        <Picker.Item label="SDG 4 - Quality Education" value="4" />
+                                        <Picker.Item label="SDG 5 - Gender Equality" value="5" />
+                                        <Picker.Item label="SDG 6 - Clean Water and Sanitation" value="6" />
+                                        <Picker.Item label="SDG 7 - Affordable and Clean Energy" value="7" />
+                                        <Picker.Item label="SDG 8 - Decent Work and Economic Growth" value="8" />
+                                        <Picker.Item label="SDG 9 - Industry, Innovation and Infrastructure" value="9" />
+                                        <Picker.Item label="SDG 10 - Reduce Inequalities" value="10" />
+                                        <Picker.Item label="SDG 11 - Sustainable Cities and Communities" value="11" />
+                                        <Picker.Item label="SDG 12 - Responsible Consumption and Production" value="12" />
+                                        <Picker.Item label="SDG 13 - Climate Action" value="13" />
+                                        <Picker.Item label="SDG 14 - Life below Water" value="14" />
+                                        <Picker.Item label="SDG 15 - Life on Land" value="15" />
+                                        <Picker.Item label="SDG 16 - Peace, Justice and Strong Institutions" value="16" />
+                                        <Picker.Item label="SDG 17 - Partnership for the Goals" value="17" />
                                     </Picker>
                                 </Text>
                                 <p/>
@@ -177,23 +177,23 @@ export default function Add ({route, navigation}){
                                         onValueChange={(value) =>
                                             setSelected2ndSDG(value)
                                         }>
-                                        <Picker.Item label="1" value="1" />
-                                        <Picker.Item label="2" value="2" />
-                                        <Picker.Item label="3" value="3" />
-                                        <Picker.Item label="4" value="4" />
-                                        <Picker.Item label="5" value="5" />
-                                        <Picker.Item label="6" value="6" />
-                                        <Picker.Item label="7" value="7" />
-                                        <Picker.Item label="8" value="8" />
-                                        <Picker.Item label="9" value="9" />
-                                        <Picker.Item label="10" value="10" />
-                                        <Picker.Item label="11" value="11" />
-                                        <Picker.Item label="12" value="12" />
-                                        <Picker.Item label="13" value="13" />
-                                        <Picker.Item label="14" value="14" />
-                                        <Picker.Item label="15" value="15" />
-                                        <Picker.Item label="16" value="16" />
-                                        <Picker.Item label="17" value="17" />
+                                        <Picker.Item label="SDG 1 - No Poverty" value="1" />
+                                        <Picker.Item label="SDG 2 - Zero Hunger" value="2" />
+                                        <Picker.Item label="SDG 3 - Good Health and Well-Being" value="3" />
+                                        <Picker.Item label="SDG 4 - Quality Education" value="4" />
+                                        <Picker.Item label="SDG 5 - Gender Equality" value="5" />
+                                        <Picker.Item label="SDG 6 - Clean Water and Sanitation" value="6" />
+                                        <Picker.Item label="SDG 7 - Affordable and Clean Energy" value="7" />
+                                        <Picker.Item label="SDG 8 - Decent Work and Economic Growth" value="8" />
+                                        <Picker.Item label="SDG 9 - Industry, Innovation and Infrastructure" value="9" />
+                                        <Picker.Item label="SDG 10 - Reduce Inequalities" value="10" />
+                                        <Picker.Item label="SDG 11 - Sustainable Cities and Communities" value="11" />
+                                        <Picker.Item label="SDG 12 - Responsible Consumption and Production" value="12" />
+                                        <Picker.Item label="SDG 13 - Climate Action" value="13" />
+                                        <Picker.Item label="SDG 14 - Life below Water" value="14" />
+                                        <Picker.Item label="SDG 15 - Life on Land" value="15" />
+                                        <Picker.Item label="SDG 16 - Peace, Justice and Strong Institutions" value="16" />
+                                        <Picker.Item label="SDG 17 - Partnership for the Goals" value="17" />
                                     </Picker>
                                 </Text>
                                 <p/>
